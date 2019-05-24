d7d6fe60b15a396fe93c9b343f755f1586763fc2
vladsaveliev <vladislav.sav@gmail.com>
2019-05-24 16:06:46 +1000

    Conda: move new R deps into meta.yaml

--- conda_pkg/cpsr/build.sh
+++ conda_pkg/cpsr/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-R -e "library(devtools); devtools::install_github('hms-dbmi/UpSetR',   dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
-R -e "library(devtools); devtools::install_github('kassambara/ggpubr', dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
-
 mkdir -p ${PREFIX}/bin
 chmod +x ${SRC_DIR}/*.py
 mv ${SRC_DIR}/*.py ${PREFIX}/bin/
