840e45f105be21231deb40c8a53fe8f53a273063
pdiakumis <peterdiakumis@gmail.com>
2024-05-16 20:24:30 +1000

    why not install openxlsx2 from cran, d'oh

--- conda/recipe/cpsr/build.sh
+++ conda/recipe/cpsr/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-${R} -e "install.packages('devtools', repos = 'https://cloud.r-project.org/', lib = '${PREFIX}/lib/R/library')"
-${R} -e "devtools::install_github(repo = 'JanMarvin/openxlsx2', ref = 'v1.6', lib = '${PREFIX}/lib/R/library')"
+${R} -e "install.packages('openxlsx2', repos = 'https://cloud.r-project.org/', lib = '${PREFIX}/lib/R/library')"
 ${R} CMD INSTALL --build . ${R_ARGS}
