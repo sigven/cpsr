21b78ec7b6760ba2b0b06a0b05c2b68fda4697fb
pdiakumis <peterdiakumis@gmail.com>
2024-05-16 20:11:42 +1000

    conda: install JanMarvin/openxlsx2 during build stage

--- conda/recipe/cpsr/build.sh
+++ conda/recipe/cpsr/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
+${R} -e "install.packages('devtools', repos = 'https://cloud.r-project.org/', lib = '${PREFIX}/lib/R/library')"
+${R} -e "devtools::install_github(repo = 'JanMarvin/openxlsx2', ref = 'v1.6', lib = '${PREFIX}/lib/R/library')"
 ${R} CMD INSTALL --build . ${R_ARGS}
