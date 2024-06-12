f07f8a28f56c43df90f5b91452080a0daf91459b
pdiakumis <peterdiakumis@gmail.com>
2024-06-12 22:55:29 +1000

    add deps: bslib, crosstalk, DT, htmltools, shiny

--- R/utils_shortcuts.R
+++ R/utils_shortcuts.R
@@ -8,3 +8,13 @@
 NULL
 
 utils::globalVariables(c("."))
+
+#' @noRd
+dummy1 <- function() {
+  # Solves R CMD check: Namespaces in Imports field not imported from
+  DT::datatable
+  htmltools::br
+  shiny::bootstrapPage
+  bslib::page_fillable
+  crosstalk::filter_select
+}
